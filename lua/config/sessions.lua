d536b1a66c8499e7ed66db75dc095af16bdb66ed
WarrenE456 <w@eavesonline.com>
2024-07-03 15:01:17 -0500

    Bug fixes

--- lua/config/sessions.lua
+++ lua/config/sessions.lua
@@ -1,2 +1,5 @@
-require("auto-session").setup {}
+require("auto-session").setup {
+    auto_session_allowed_dirs = { 'dev' },
+    auto_session_create_enabled = false,
+}
 require("telescope").load_extension("session-lens")
