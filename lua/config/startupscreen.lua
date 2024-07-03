8648641a1917b40c8817e8e85a372dc088aafe3f
WarrenE456 <w@eavesonline.com>
2024-07-03 12:06:00 -0500

    Edit start up screen

--- lua/config/startupscreen.lua
+++ lua/config/startupscreen.lua
@@ -1,1 +1,57 @@
-require'alpha'.setup(require'alpha.themes.startify'.config)
+local alpha = require("alpha") local dashboard = require("alpha.themes.dashboard")
+
+-- Set header
+dashboard.section.header.val = {
+"              .........",
+"            .'------.' |",
+"           | .-----. | |",
+"           | |1    | | |",
+"         __| |~    | | |;. _______________",
+"        /  |*`-----'.|.' `;              //",
+"       /   `---------' .;'              //",
+" /|   /  .''''////////;'               //",
+"|=|  .../ ######### /;/               //|",
+"|/  /  / ######### //                //||",
+"   /   `-----------'                // ||",
+"  /________________________________//| ||",
+"  `--------------------------------' | ||",
+"   : | ||      | || |__LL__|| ||     | ||",
+"   : | ||      | ||         | ||     `\"\"\'",
+"   n | ||      `\"\"\'         | ||",
+"   M | ||                   | ||",
+"     | ||                   | ||",
+"     `\"\"\'                   `\"\"\'",
+}
+
+-- Set menu
+dashboard.section.buttons.val = {
+    dashboard.button( "ff", "?  > Find file", ":cd ~/dev | Telescope find_files<CR>"),
+    dashboard.button( "fs", "q > Find sessions", ":SearchSession <CR>"),
+    dashboard.button( "r", "  > Recent"   , ":Telescope oldfiles<CR>"),
+    dashboard.button( "s", "  > Config" , ":cd ~/.config/nvim | e init.lua <CR>"),
+}
+
+-- Set footer
+--   NOTE: This is currently a feature in my fork of alpha-nvim (opened PR #21, will update snippet if added to main)
+--   To see test this yourself, add the function as a dependecy in packer and uncomment the footer lines
+--   ```init.lua
+--   return require('packer').startup(function()
+--       use 'wbthomason/packer.nvim'
+--       use {
+--           'goolord/alpha-nvim', branch = 'feature/startify-fortune',
+--           requires = {'BlakeJC94/alpha-nvim-fortune'},
+--           config = function() require("config.alpha") end
+--       }
+--   end)
+--   ```
+-- local fortune = require("alpha.fortune") 
+-- dashboard.section.footer.val = fortune()
+
+-- Send config to alpha
+alpha.setup(dashboard.opts)
+
+-- Disable folding on alpha buffer
+vim.cmd([[
+    autocmd FileType alpha setlocal nofoldenable
+]])
+
